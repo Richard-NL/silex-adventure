120bbae3f578031db7d651cbe5c2fa4467e7e966
Richard Hoogstad <rhoogstad@saa.nl>
2016-12-11 18:52:44 +0100

    C'mon this needs to be in there with the last one

--- src/Rsh/Adventure/Command/AdventureCommand.php
+++ src/Rsh/Adventure/Command/AdventureCommand.php
@@ -2,14 +2,15 @@
 namespace Rsh\Adventure\Command;
 
 use Knp\Command\Command;
-use Rsh\Adventure\InputHelper\ActionStrategy;
+
+use Rsh\Adventure\Action\ActionStrategy;
+use Rsh\Adventure\Action\ExitAction;
+use Rsh\Adventure\Action\GoToAction;
 use Rsh\Adventure\Service\DirectionServiceClient;
-use Rsh\Adventure\InputHelper\InputHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Question\Question;
-
+use Rsh\Adventure\Action\Subject\NoSubject;
 
 class AdventureCommand extends Command
 {
@@ -18,7 +19,10 @@ class AdventureCommand extends Command
     public function __construct($name = null)
     {
         parent::__construct($name);
-        $this->directionServiceClient = DirectionServiceClient::create();
+
+
+        // @todo di container?
+        // $this->locationServiceClient = LocationServiceClient::create();
     }
 
     protected function configure()
@@ -31,17 +35,55 @@ class AdventureCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $helper = $this->getHelper('question');
-        $inputHelper = new InputHelper();
-        $currentLocation = 'Muddy Beach';
-        
-        question:
-        $question = new Question('>', false);
+        // @todo di container?
+        $directionServiceClient = DirectionServiceClient::create();
 
-        $userInputText = trim($helper->ask($input, $output, $question));
-        $actionHandler = new ActionStrategy($inputHelper);
-        $action = $actionHandler->getAction($userInputText);
-        $output->writeln(get_class($action) . ' instance found');
-        
-        goto question;
+        $currentLocation = 'A large hole on the Surface';
+
+        $exitCommand = false;
+        while (!$exitCommand) {
+            $question = new Question('>', false);
+
+            $userInputText = trim($helper->ask($input, $output, $question));
+
+            // @todo di container?
+            $actionStrategy = new ActionStrategy();
+            $action = $actionStrategy->getAction($userInputText);
+            $output->writeln(get_class($action) . ' instance found');
+
+            // @todo refactor to ActionHandler class?
+            $exitCommand = (function() use ($action, $output) {
+                if ($action instanceof ExitAction) {
+
+                    $output->writeln('Exiting program, goodbye');
+                    return true;
+                }
+                return false;
+            })();
+            if ($exitCommand) {
+                break;
+            }
+
+            // @todo refactor to ActionHandler class?
+            (function() use ($action, $output, $directionServiceClient, $currentLocation) {
+                if ($action instanceof GoToAction === false) {
+                    return;
+                }
+
+                if ($action->getSubject()->getName() instanceof NoSubject) {
+                    $output->writeln('Where do you want to go?');
+                    $directionsNames = $directionServiceClient->getDirections($currentLocation);
+                    foreach ($directionsNames as $directionName) {
+                        $output->writeln($directionName);
+                    }
+
+                    return;
+                }
+
+                $output->writeln('Going to location ' . $action->getSubject()->getName() . ' (Well not realy)');
+                return;
+
+            })();
+        }
     }
 }
--- test/Adventure/Action/ActionStrategyTest.php
+++ test/Adventure/Action/ActionStrategyTest.php
@@ -1,47 +1,29 @@
 <?php
 
-namespace Rsh\Adventure\InputHelper;
+namespace Rsh\Adventure\Action;
 
-
-
-use Prophecy\Argument;
-use Rsh\Adventure\Action\ExitAction;
-use Rsh\Adventure\Action\GoToAction;
-use Rsh\Adventure\Action\InventoryAction;
-use Rsh\Adventure\Action\NoAction;
 
 class ActionStrategyTest extends \PHPUnit_Framework_TestCase
 {
     public function getActionDataProvider(): array
     {
         return [
-            [$this->mockInputHelper(true, false, false, false), ExitAction::class, 'exit'],
-            [$this->mockInputHelper(false, true, false, false), InventoryAction::class, 'inventory'],
-            [$this->mockInputHelper(false, false, true, false), GoToAction::class, 'go to'],
-            [$this->mockInputHelper(false, false, false, true), GoToAction::class, 'go to Foo'],
-            [$this->mockInputHelper(false, false, false, false), NoAction::class, ''],
-            [$this->mockInputHelper(false, false, false, false), NoAction::class, 'bogus'],
+            [ExitAction::class, 'exit'],
+            [InventoryAction::class, 'inventory'],
+            [GoToAction::class, 'go to'],
+            [GoToAction::class, 'go to Foo'],
+            [NoAction::class, ''],
+            [NoAction::class, 'bogus'],
         ];
-    }
-
-    private function mockInputHelper(bool $exitTyped, bool $inventoryTyped, bool $goToTyped, bool $goToWithDirectionTyped): InputHelper
-    {
-        $inputHelper = $this->prophesize(InputHelper::class);
-
-        $inputHelper->isExitTyped(Argument::any())->willReturn($exitTyped);
-        $inputHelper->isInventoryTyped(Argument::any())->willReturn($inventoryTyped);
-        $inputHelper->isGoToTyped(Argument::any())->willReturn($goToTyped);
-        $inputHelper->isGoToWithDirectionTyped(Argument::any())->willReturn($goToWithDirectionTyped);
-        return $inputHelper->reveal();
     }
 
 
     /**
      * @dataProvider getActionDataProvider
      */
-    public function testGetAction($inputHelper, $expected, $text)
+    public function testGetAction($expected, $text)
     {
-        $action = (new ActionStrategy($inputHelper))->getAction($text);
+        $action = (new ActionStrategy())->getAction($text);
         $this->assertSame($expected, get_class($action));
     }
 }
